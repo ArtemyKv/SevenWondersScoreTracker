20aa85bc2ffd7c311f9d97ebe816bb55f24af812
ArtemKvashnin <artemykv@gmail.com>
2023-07-15 18:15:05 +0500

    add project to repo

---
+++ SevenWondersScoreTracker/SevenWondersScoreTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenWondersScoreTrackerApp.swift
+//  SevenWondersScoreTracker
+//
+//  Created by Artem Kvashnin on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SevenWondersScoreTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
